36f26f90160907af0712222bf52f0e08643895bd
Denis <den-282008@yandex.ru>
2021-03-17 00:14:27 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Header} from "./ui/1-header/Header";
+import {Content} from "./ui/2-content/Content";
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
