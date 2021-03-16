56140deb08e746ca30de79893a26b7bc795919b7
Denis <den-282008@yandex.ru>
2021-03-17 02:13:15 +0300

    Realize render user boards, and functions add board and delete board

---
+++ src/api/boardsAPI.ts
@@ -0,0 +1,15 @@
+import {instance} from "./instance";
+import {BoardType} from "../redux/reducers/boards-reducer";
+
+
+export const boardsAPI = {
+  getBoards() {
+    return instance.get("/boards").then(res => res.data);
+  },
+  addBoard(newBoard: BoardType) {
+    return instance.post("/boards", newBoard).then(res => res.data);
+  },
+  deleteBoard(id: string) {
+    return instance.delete(`boards/${id}`);
+  }
+};
